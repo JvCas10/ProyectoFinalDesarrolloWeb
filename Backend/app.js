b524d9a446f208b209cbfad6199f95e5222d613b
JvCas10 <jcastellanosp3@miumg.edu.gt>
2024-10-15 18:28:10 -0600

    Agregando paquetes Mongoose, Express, Jsonwebtoken, Cors

--- Backend/app.js
+++ Backend/app.js
@@ -1,1 +1,30 @@
-console.log("hola mundo");
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors'); // Importar el paquete cors
+const app = express();
+const port = 3001;
+const usuarioRoutes = require('./routes/usuario.routes');
+
+// Middleware para habilitar CORS
+app.use(cors({
+    origin: 'http://localhost:3000', 
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+    allowedHeaders: ['Content-Type', 'Authorization'], 
+}));
+
+app.use(express.json());
+app.use('/api', usuarioRoutes);
+
+app.get('/', (req, res) => {
+    res.send('Hello world!');
+});
+
+app.listen(port, () => {
+    console.log('El servidor está listo');
+});
+
+mongoose.connect('mongodb://localhost:27017/miBaseDeDatos').then(() => {
+    console.log('Conexión a MongoDB exitosa');
+}).catch((error) => {
+    console.error('Error al conectar a MongoDB:', error);
+});
